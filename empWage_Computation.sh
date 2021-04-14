5d5ffe8fcd653b6cccb07741bb7e1cdbd25d9724
prudhvi <prudhvieethakota123@gmail.com>
2021-04-15 00:08:09 +0530

    Added:Added PartTime Employee and Wage

--- empWage_Computation.sh
+++ empWage_Computation.sh
@@ -3,15 +3,20 @@ echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash 
 
 isPresent=1
+isPartTime=2
 isAbsent=0
 wagePerHr=20
 fullDayHours=8
+partTimeHours=8
 
-randomNumber=$((RANDOM%2))
+randomNumber=$((RANDOM%3))
 if [ $randomNumber -eq $isPresent ]
 then 
     echo "Employee is Present"
     dailyEmployeeWage=$(($fullDayHours*$wagePerHr))
+elif [ $randomNumber -eq $isPartTime ]
+then
+    dailyEmployeeWage=$(($partTimeHours*$wagePerHr))
 else
     echo "Employee is Absent"
     dailyEmployeeWage=0
